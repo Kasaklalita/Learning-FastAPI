df72b711906e7b31e8c84a94b7d79f47da905806
Veniamin Polienko <kasaklalita@mail.ru>
2023-01-08 23:32:20 +0300

    Path parameters

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'data': {
-        'name': 'Veniamin'
-    }}
+    return {'data': 'blog list'}
 
-@app.get('/about')
-def about():
-    return {'data': 'about'}
+
+@app.get('/blogs/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blogs/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+@app.get('/blogs/{id}/comments')
+def comments(id):
+    return {'data': 'comments'}
