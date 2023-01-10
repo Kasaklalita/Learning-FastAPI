5e52bbc8e1bb2d608a078729e9b8f3e9439c3039
Veniamin Polienko <kasaklalita@mail.ru>
2023-01-10 15:28:55 +0300

    Created a model and a table

--- blog/main.py
+++ blog/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from . import schemas
-
+from . import models, schemas
+from .database import engine
 
 app = FastAPI()
+
+models.Base.metadata.create_all(engine)
 
 
 @app.post('/blogs')
