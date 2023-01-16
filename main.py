f0b2aa279b27120d63e1adaa6f35732996c3f4dd
Veniamin Polienko <kasaklalita@mail.ru>
2023-01-16 17:44:41 +0300

    Query parameters

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'Veniamin'}}
-
-
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+@app.get('/blogs')
+def index(limit=10, published=True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
 
 
 @app.get('/blogs/{id}')
