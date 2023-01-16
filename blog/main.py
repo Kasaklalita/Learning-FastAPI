ff34df97819559f99ff750bd6befd02c777ab19f
Veniamin Polienko <kasaklalita@mail.ru>
2023-01-16 22:06:06 +0300

    Creating users

--- blog/main.py
+++ blog/main.py
@@ -3,6 +3,7 @@ from . import models, schemas
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from typing import List
+from .hashing import Hash
 
 models.Base.metadata.create_all(engine)
 
@@ -63,3 +64,23 @@ def show(id, response: Response, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'Blog with the id  {id} is not available')
     return blog
+
+
+@app.post('/users/', response_model=schemas.ShowUser)
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+
+    new_user = models.User(
+        name=request.name, email=request.email, password=Hash.brypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get("/users/{id}", response_model=schemas.ShowUser)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'User with the id  {id} is not available')
+    return user
--- blog/schemas.py
+++ blog/schemas.py
@@ -12,3 +12,17 @@ class ShowBlog(BaseModel):
 
     class Config():
         orm_mode = True
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+
+    class Config():
+        orm_mode = True
