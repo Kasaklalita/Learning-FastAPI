098f56bcc0910085ad5bda1c3f4d0203000afb82
Veniamin Polienko <kasaklalita@mail.ru>
2023-01-16 19:28:11 +0300

    Added DELETE and PUT methods

--- blog/main.py
+++ blog/main.py
@@ -25,6 +25,28 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
+@app.delete('/blogs/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {id} not found')
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+
+@app.put('/blogs/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {id} not found')
+    blog.update(request.dict())
+    db.commit()
+    return 'updated'
+
+
 @app.get('/blogs')
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
