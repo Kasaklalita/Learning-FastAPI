c75e29f22f90d98fbd8fde769b9c32d35e746b64
Veniamin Polienko <kasaklalita@mail.ru>
2023-01-16 17:35:38 +0300

    Install & setup

--- main.py
+++ main.py
@@ -1,45 +1,13 @@
 from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
-import uvicorn
 
 app = FastAPI()
 
 
-@app.get('/blogs')
-def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
-    # only get 10 published blogs
-    if published:
-        return {'data': f'{limit} published blogs from the database'}
-    else:
-        return {'data': f'{limit} blogs from the database'}
+@app.get('/')
+def index():
+    return {'data': {'name': 'Veniamin'}}
 
 
-@app.get('/blogs/unpublished')
-def unpublished():
-    return {'data': 'all unpublished blogs'}
-
-
-@app.get('/blogs/{id}')
-def show(id: int):
-    return {'data': id}
-
-
-@app.get('/blogs/{id}/comments')
-def comments(id, limit = 10):
-    return limit
-
-
-class Blog(BaseModel):
-    title: str
-    body: str
-    published: Optional[bool]
-
-
-@app.post('/blogs')
-def create_blog(blog: Blog):
-    return {'data': f'Blog is created with {blog.title}'}
-
-
-# if __name__ == "__name__":
-#     uvicorn.run(app, host="127.0.0.1", port=9000)
+@app.get('/about')
+def about():
+    return {'data': 'about page'}
