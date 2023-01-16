fe050713470cf8daf3ec8f20d328322008a7832b
Veniamin Polienko <kasaklalita@mail.ru>
2023-01-16 18:43:08 +0300

    Added GET and POST operations

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import models, schemas
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(engine)
 
 app = FastAPI()
 
 
-@app.post('/blogs')
-def create(blog: schemas.Blog):
-    return {'title': blog.title, 'body': blog.body}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post('/blogs', status_code=201)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get('/blogs')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blogs/{id}')
+def show(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
