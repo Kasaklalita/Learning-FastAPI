73544e188dc1a932ae15b12153d223ada5d65dea
Veniamin Polienko <kasaklalita@mail.ru>
2023-01-21 19:39:17 +0300

    Dependencies in path operation decorators

--- dependencies/main.py
+++ dependencies/main.py
@@ -1,20 +1,19 @@
-from fastapi import Depends, FastAPI, Cookie
+from fastapi import Depends, FastAPI, Header, HTTPException
 
 app = FastAPI()
 
 
-def query_extractor(q: str | None = None):
-    return q
+async def verify_token(x_token: str = Header()):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
 
 
-def query_or_cookie_extractor(
-    q: str = Depends(query_extractor), last_query: str | None = Cookie(default=None)
-):
-    if not q:
-        return last_query
-    return q
+async def verify_key(x_key: str = Header()):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
 
 
-@app.get("/items/")
-async def read_query(query_or_default: str = Depends(query_or_cookie_extractor)):
-    return {"q_or_cookie": query_or_default}
+@app.get("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
+async def read_items():
+    return [{"item": "Foo"}, {"item": "Bar"}]
